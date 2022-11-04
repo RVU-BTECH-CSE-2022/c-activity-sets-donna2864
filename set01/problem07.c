a7d3583991c903b81d642ee8c84498f92c61cc3d
Donna R <112607918+donna2864@users.noreply.github.com>
2022-11-04 05:17:32 +0000

    problem07 complete

--- set01/problem07.c
+++ set01/problem07.c
@@ -3,6 +3,24 @@ int input_n();
 int sum_n_nos(int n);
 void output(int n, int sum);
 int main(){
-  int n, sum;
-  
+  int n,i, sum=0;
+  printf("enter the 'n' no. of terms you want to calculate:\n");
+  n = input_n();
+  sum = sum_n_nos(n);
+  output(n, sum);
+}
+int input_n(){
+  int n;
+  scanf("%d", &n);
+  return n;
+}
+int sum_n_nos(int n){
+  int i, sum;
+  for(int i = 0; i <= n; i++){
+    sum += i;
+  }
+  return sum;
+}
+void output(int n, int sum){
+  printf("the sum of %d terms is %d\n", n, sum);
 }
