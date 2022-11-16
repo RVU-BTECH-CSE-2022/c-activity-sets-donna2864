09c32f76dda74e7f96e6d45c47fd58bcf55aec36
Donna R <112607918+donna2864@users.noreply.github.com>
2022-11-16 05:35:49 +0000

    problem10 complete

--- set01/problem10.c
+++ set01/problem10.c
@@ -7,13 +7,36 @@ int main(){
   int result;
  input_two_strings(string1, string2);
  result = stringcompare(string1, string2);
+ output(string1, string2, result);
 }
 void input_two_strings(char *string1, char *string2){
   printf("Enter the first string: ");
-  scanf("%s", *string1);
+  scanf("%s", string1);
   printf("Enter the second string: ");
-  scanf("%s", *string2);
+  scanf("%s", string2);
 }
 int stringcompare(char *string1, char *string2){
-  
+  int i = 0;
+  while(*string1 != '\0' && *string2 != '\0')
+    {
+      if (*string1 != *string2)
+      {
+        i = 1;
+      }
+      string1++;
+      string2++;
+    }
+  if(i==0){
+    return 0;
+  }
+  else {
+    return 1;
+  }
+}
+void output(char *string1, char *string2, int result){
+  if (result == 0){
+    printf("\"%s\" is same as \"%s\"", string1, string2);}
+  else{
+    printf("\"%s\" is greater than \"%s\"", string2, string1);
+  }
 }
